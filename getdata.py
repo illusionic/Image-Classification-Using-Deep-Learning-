14d13a96121d5a4e80df8fc42112c142abbd6b98
Yasrub Bashir <evasionic@gmail.com>
2019-03-01 23:23:38 +0500

    Add files via upload

---
+++ getdata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 12:28:05 2019
+
+@author: Illusion
+"""
+import h5py
+from sklearn.model_selection import train_test_split
+
+def load():
+    f = h5py.File("dataset.h5")
+    x = f['x'].value
+    y = f['y'].value
+    f.close()
+    x_train , x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=100)
+    return x_train, x_test, y_train, y_test
+    
